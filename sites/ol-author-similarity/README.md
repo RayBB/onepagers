f121c263debb3be878422698aea93a147278928f
Ray Berger <RayBB@users.noreply.github.com>
2021-07-30 15:53:53 -0400

    add Open Library author similarities

---
+++ sites/ol-author-similarity/README.md
@@ -0,0 +1,6 @@
+Just want a quick way to find duplicate works by an author on Open Library.
+
+Uses [string-similarity](https://www.npmjs.com/package/string-similarity) to find works with > .9 similarity;
+
+If people actually end up using this maybe I'll convert to Vue and make it a little nicer.
+
---
+++ sites/ol-author-similarity/index.js
@@ -0,0 +1,74 @@
+function getAuthorWorks(authorId) {
+    return fetch(`https://openlibrary.org/authors/${authorId}/works.json?limit=1000`)
+      .then(response => response.json())
+  }
+
+function getSimilarWorksByTitle(mainWork, allWorks, similarityThreshold = .9){
+    // main work is the one we want to find similar works to
+    const worksExcludingMainWork = allWorks.filter(work => work !== mainWork);
+    const titles = worksExcludingMainWork.map(work => work.title);
+
+    const similarities = stringSimilarity.findBestMatch(mainWork.title, titles);
+    const similarWorks = similarities.ratings
+    .map((result, index)=>{
+        if (result.rating > similarityThreshold) {
+            return worksExcludingMainWork[index];
+        }
+    })
+    .filter(work => work !== undefined);
+
+    similarWorks.push(mainWork);
+    return {
+        maxSimilarity: similarities.bestMatch.rating,
+        works: similarWorks
+    }
+}
+  
+
+function findGroupsOfSimilar(authorWorks, similarityThreshold = .9){
+    const groups = [];
+    let entries = authorWorks.entries;
+    console.log("Entry count:", entries.length);
+    while (entries.length > 1) {
+        const mainWork = entries.shift();
+        const similarWorks = getSimilarWorksByTitle(mainWork, entries, similarityThreshold);
+        if (similarWorks.maxSimilarity > similarityThreshold) {
+            console.log(similarWorks,
+                similarWorks.works.map(work => work.title),
+                similarWorks.works.map(work => work.key.replace("/works/","")).join(',')
+                );
+            groups.push(similarWorks);
+            entries = entries.filter(entry => !similarWorks.works.includes(entry));
+        }
+    }
+    return groups;
+}
+
+function buttonClicked(){
+    const authorId = document.getElementById("authorId").value;
+    console.log("searching for works by", authorId);
+
+    getAuthorWorks(authorId)
+    .then(authorWorks => {
+        const groups = findGroupsOfSimilar(authorWorks);
+        document.getElementById("results").innerHTML = generateResultsHTML(groups, authorId)
+    })
+}
+function generateResultsHTML(groups, authorId){
+    let html = "";
+    groups.forEach(group => {
+        html += "<ul>"
+        group.works.forEach(work => {
+            const workId = work.key.replace("/works/","");
+            html += `<li>${work.title} (<a href="https://openlibrary.org/works/${workId}/">${workId}</a>)</li>`
+        })
+        html += "</ul>"
+        const query = group.works.map(work => work.key.replace("/works/","")).join('+OR+')
+        html += `<a href="https://openlibrary.org/authors/${authorId}/?q=${query}">Search for these works</a>`
+        html += "<hr>"
+    })
+    return html
+}
+const searchButton = document.getElementById("searchBtn");
+console.log(searchButton)
+searchButton.addEventListener("click", e=>buttonClicked(e));
