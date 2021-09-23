d45db7293576dea03a91f6b342154f0949f0a103
Ray Berger <RayBB@users.noreply.github.com>
2021-09-22 22:06:20 -0400

    vue.html to index.html

--- sites/ol-author-similarity/index.js
+++ sites/ol-author-similarity/index.js
@@ -1,100 +1,193 @@
-function getAuthorWorks(authorId) {
-    return fetch(`https://openlibrary.org/authors/${authorId}/works.json?limit=1000`)
-      .then(response => response.json())
-  }
-
-function getSimilarWorksByTitle(mainWork, allWorks, similarityThreshold = .9){
-    // main work is the one we want to find similar works to
-    const worksExcludingMainWork = allWorks.filter(work => work !== mainWork);
-    const titles = worksExcludingMainWork.map(work => work.title.toLowerCase());
-
-    const similarities = stringSimilarity.findBestMatch(mainWork.title.toLowerCase(), titles);
-    const similarWorks = similarities.ratings
-    .map((result, index)=>{
-        if (result.rating > similarityThreshold) {
-            return worksExcludingMainWork[index];
+const Counter = {
+    data() {
+        return {
+            authorIdTextBox: 'OL6621416A', // this is the value of the box and changes as the box changes
+            authorId: 'OL6621416A', // authorID that is currently being searched/shown. The internal representation after go clicked
+            status: '',
+            authorWorksJson: {},
+            authorJson: {},
+            includeSubtitles: true, // should subtitles be included in similarity check
+            aggressiveNormalization: true,
+            settingsVisible: true,
+            coversVisible: true,
+            authorVisible: true,
+            dataToRemember: ['includeSubtitles', 'aggressiveNormalization', 'settingsVisible', 'coversVisible'],
+            // TODO: configurable similarity threshold, disable cache, two columns of settings
+            // skip to author with similar works
+            // add count of authors with search results of similar names
         }
-    })
-    .filter(work => work !== undefined);
+    },
+    mounted() {
+        // Automatically load data if authorId specified in URL
+        const queryParams = new URLSearchParams(window.location.search);
+        if (queryParams.get("authorId")) {
+            this.authorId = queryParams.get("authorId");
+        }
+    },
+    created() {
+        for (const dataName of this.dataToRemember) {
+            const storedValue = localStorage.getItem(dataName);
+            if (storedValue !== null) {
+                // we stringify and parse to get boolean values and probably arrays one day too
+                this[dataName] = JSON.parse(storedValue);
+            }
+            this.$watch(dataName, (newVal, oldVal) => {
+                localStorage.setItem(dataName, JSON.stringify(newVal));
+            })
+        }
+    },
+    watch: {
+        async authorId(newValue) {
+            this.authorIdTextBox = newValue;
+            // set URL
+            const url = new URL(window.location);
+            url.searchParams.set('authorId', newValue);
+            window.history.replaceState(null, '', url.toString());
+            this.status = `${this.authorId} - searching`;
+            this.getAuthor();
+            this.authorWorksJson = await this.getAuthorWorks();
+        }
+    },
+    computed: {
+        groupsOfSimilarWorks() {
+            const groups = [];
+            const similarityThreshold = .9;
+            if (!('entries' in this.authorWorksJson)) {
+                return []
+            }
+            // A hack to deep clone
+            let entries = JSON.parse(JSON.stringify(this.authorWorksJson.entries));
+            console.log("Entry count:", entries.length);
+            while (entries.length > 1) {
+                const mainWork = entries.shift();
+                const similarWorks = this.getSimilarWorksByTitle(mainWork, entries, similarityThreshold);
+                if (similarWorks.maxSimilarity > similarityThreshold) {
+                    console.log(similarWorks,
+                        similarWorks.works.map(work => work.title),
+                        similarWorks.works.map(work => work.key.replace("/works/", "")).join(',')
+                    );
+                    groups.push(similarWorks);
+                    entries = entries.filter(entry => !similarWorks.works.includes(entry));
+                }
+            }
+            return groups;
+        },
+        authorIdNumber() {
+            return parseInt(this.authorId.match(/\d+/)[0]);
+        },
+        authorFieldsToDisplay() {
+            const toDisplay = [];
+            if (this.authorId) {
+                toDisplay.push({
+                    "description": "ID",
+                    "value": `<a target="_blank" href="https://openlibrary.org/authors/${this.authorId}">${this.authorId}</a>`
+                })
+            }
+            if (this.authorJson.name) {
+                toDisplay.push({"description": "Name", "value": this.authorJson.name})
+            }
+            if (this.authorJson.name) {
+                toDisplay.push({"description": "Works", "value": this.authorWorksJson?.entries?.length || '...'})
+            }
+            if (this.authorJson.birth_date || this.authorJson.death_date) {
+                toDisplay.push({
+                    "description": "Lived",
+                    "value": `${this.authorJson.birth_date || ''} - ${this.authorJson.death_date || ''}`
+                })
+            }
+            if (this.authorJson?.remote_ids?.wikidata) {
+                toDisplay.push({
+                    "description": "Wikidata",
+                    "value": `<a target="_blank" href="https://www.wikidata.org/wiki/${this.authorJson.remote_ids.wikidata}">${this.authorJson.remote_ids.wikidata}</a>`
+                })
+            }
+            return toDisplay;
+        }
+    },
+    methods: {
+        getSimilarWorksByTitle(mainWork, allWorks, similarityThreshold = .9) {
+            // main work is the one we want to find similar works to
+            const worksExcludingMainWork = allWorks.filter(work => work !== mainWork);
+            const titles = worksExcludingMainWork.map(work => this.getTitleFromWork(work).toLowerCase());
 
-    similarWorks.push(mainWork);
-    return {
-        maxSimilarity: similarities.bestMatch.rating,
-        works: similarWorks
-    }
-}
+            const similarities = stringSimilarity.findBestMatch(this.getTitleFromWork(mainWork).toLowerCase(), titles);
+            const similarWorks = similarities.ratings
+                .map((result, index) => {
+                    if (result.rating > similarityThreshold) {
+                        return worksExcludingMainWork[index];
+                    }
+                })
+                .filter(work => work !== undefined);
 
-
-function findGroupsOfSimilar(authorWorks, similarityThreshold = .9){
-    const groups = [];
-    let entries = authorWorks.entries;
-    console.log("Entry count:", entries.length);
-    while (entries.length > 1) {
-        const mainWork = entries.shift();
-        const similarWorks = getSimilarWorksByTitle(mainWork, entries, similarityThreshold);
-        if (similarWorks.maxSimilarity > similarityThreshold) {
-            console.log(similarWorks,
-                similarWorks.works.map(work => work.title),
-                similarWorks.works.map(work => work.key.replace("/works/","")).join(',')
-                );
-            groups.push(similarWorks);
-            entries = entries.filter(entry => !similarWorks.works.includes(entry));
+            similarWorks.push(mainWork);
+            return {
+                maxSimilarity: similarities.bestMatch.rating,
+                works: similarWorks
+            }
+        },
+        getTitleFromWork(work) {
+            let title = work.title;
+            if (this.includeSubtitles && work.subtitle) {
+                title += ` ${work.subtitle}`
+            }
+            if (this.aggressiveNormalization) {
+                title = title.replace(/[^A-Za-z0-9 ]/, '')
+                const stopWords = ["the", "and", "at"]; // words to be removed
+                for (const word of stopWords) {
+                    title = title.replaceAll(` ${word} `, " ")
+                }
+            }
+            return title;
+        },
+        async goBtnClicked() {
+            // TODO: this should grab authorID even from a URL or if there are spaces
+            this.authorId = this.authorIdTextBox;
+        },
+        parseKey(s) {
+            return s.split("/")[2];
+        },
+        getWorkIds(works) {
+            return works.map(work => this.parseKey(work.key));
+        },
+        getAuthor(authorId = this.authorId) {
+            this.authorJson = {};
+            fetch(`https://openlibrary.org/authors/${authorId}.json`, {cache: "force-cache"})
+                .then(async response => {
+                    if (!response.ok) {
+                        return {}
+                    }
+                    this.authorJson = await response.json();
+                })
+        },
+        getAuthorWorks(authorId = this.authorId) {
+            this.authorWorksJson = {};
+            return fetch(`https://openlibrary.org/authors/${authorId}/works.json?limit=1000`, {cache: "force-cache"})
+                .then(response => {
+                    if (!response.ok) {
+                        // using app here since "this" in a call back doesn't refer to the vue instance
+                        app.status = "Error requesting works"
+                        return {}
+                    }
+                    app.status = "loaded"
+                    return response.json()
+                })
+        },
+        increaseAuthorId(amount) {
+            this.authorId = `OL${this.authorIdNumber + amount}A`;
+        },
+        setRandomAuthorId() {
+            const highestAuthorId = 9500000; // This is an approximation and should increase over time
+            const randomNumber = Math.floor(Math.random() * highestAuthorId) + 1;
+            this.authorId = `OL${randomNumber}A`;
+        },
+        updateClipboard(newClip) {
+            navigator.clipboard.writeText(newClip).then(() => {
+            }, () => {
+                /* clipboard write failed */
+                app.status = "copy to clipboard failed, please check permissions"
+            });
         }
     }
-    return groups;
 }
 
-function getAuthorId(){
-    return document.getElementById("authorId").value.match(/OL\d+A/gi)[0].toUpperCase();
-}
-
-function goBtnClicked(){
-    authorId = getAuthorId()
-    console.log("searching for works by", authorId);
-    document.getElementById("results").innerHTML = `<p>${authorId} - searching</p>`;
-    getAuthorWorks(authorId)
-    .then(authorWorks => {
-        const groups = findGroupsOfSimilar(authorWorks);
-        document.getElementById("results").innerHTML = generateResultsHTML(groups, authorId);
-        document.getElementById("author-link").innerHTML = generateShowAuthorHTML(authorId);
-    })
-}
-
-function generateShowAuthorHTML(authorId) {
-  return `<a href="https://openlibrary.org/authors/${authorId}">${authorId}</a>`;
-}
-
-function generateResultsHTML(groups, authorId){
-    let html = "";
-    if (groups.length == 0){
-        html = `<p>${authorId} - no similar works found</p>`;
-    }
-    groups.forEach(group => {
-        html += "<ul>"
-        group.works.forEach(work => {
-            const workId = work.key.replace("/works/","");
-            html += `<li>${work.title} (<a target="_blank" href="https://openlibrary.org/works/${workId}/">${workId}</a>)</li>`
-        })
-        html += "</ul>"
-        const workIds = group.works.map(work => work.key.replace("/works/",""))
-        html += `<a target="_blank" href="https://openlibrary.org/authors/${authorId}/?q=${workIds.join('+OR+')}">Search</a> | `
-        html += `<a target="_blank" href="https://testing.openlibrary.org/works/merge?records=${workIds.join(',')}">Merge</a> | `
-        html += `<span>${workIds.join(',')}</span>`
-        html += "<hr>"
-    })
-    return html
-}
-
-function goToPrev(){
-    document.getElementById("authorId").value = "OL" + (parseInt(getAuthorId().match(/\d+/)[0])-1) + "A";
-    goBtnClicked();
-}
-
-function goToNext(){
-    document.getElementById("authorId").value = "OL" + (parseInt(getAuthorId().match(/\d+/)[0])+1) + "A";
-    goBtnClicked();
-}
-
-document.getElementById("prevBtn").addEventListener("click", goToPrev);
-document.getElementById("nextBtn").addEventListener("click", goToNext);
-document.getElementById("searchBtn").addEventListener("click", goBtnClicked);
+const app = Vue.createApp(Counter).mount('#app')
